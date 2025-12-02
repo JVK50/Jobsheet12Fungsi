16ace9f47f3950c40d86d8917802acc874082abc
JVK50 <jidanali32@gmail.com>
2025-12-02 23:10:21 +0700

    Percobaan 4

---
+++ pengunjungKafe13.java
@@ -0,0 +1,12 @@
+public class pengunjungKafe13 {
+    static void daftarPengunjung(String...namaPengunjung) {
+        System.out.println("Daftar nama pengunjung: ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("-" + namaPengunjung[i]);
+        }
+    }
+    public static void main(String[] args) {
+        daftarPengunjung("andi");
+        daftarPengunjung("ali", "budi", "citra");
+    }
+}
