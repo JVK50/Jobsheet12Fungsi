e18901d575d89da1e64a025e75be8371698d279e
JVK50 <jidanali32@gmail.com>
2025-12-03 08:40:47 +0700

    Percobaan 5

--- hitungBalok13.java
+++ hitungBalok13.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
+
 public class hitungBalok13 {
     static int hitungLuas(int pjg, int lbr) {
-        int luas = pjg * lbr;
-        return luas;
+        int Luas = pjg * lbr;
+        return Luas;
     }
-    static int hitungVolume(int a, int b, int tgi) {
-        int volume = hitungLuas(a, b) * tgi;
+    static int hitungVolume(int tinggi, int a, int b) {
+        int volume = hitungLuas(a, b) * tinggi;
         return volume;
     }
     public static void main(String[] args) {
-        Scanner jvk = new Scanner(System.in);
+        Scanner balok = new Scanner(System.in);
         int p, l, t, L, vol;
 
         System.out.print("Masukkan panjang balok: ");
-        p = jvk.nextInt();
+        p = balok.nextInt();
         System.out.print("Masukkan lebar balok: ");
-        l = jvk.nextInt();
+        l = balok.nextInt();
         System.out.print("Masukkan tinggi balok: ");
-        t = jvk.nextInt();
+        t = balok.nextInt();
 
-        L = hitungLuas(p, l);
-        vol = hitungVolume(p, l, t);
-        
-        System.out.println("Luas permukaan balok: " + L);
-        System.out.println("Volume balok: " + vol);
-
-        jvk.close();
+        L = hitungLuas (p, l);
+        System.out.println("Luas alas balok adalah: " + L);
+        vol = hitungVolume (t, p, l);
+        System.out.println("Volume balok adalah: " + vol);
+        balok.close();
     }
+
 }
